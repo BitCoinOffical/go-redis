123d33c8b7be69bf94d027ad2cb8753441f6addf
BitCoinOffical <jlucehok228@gmail.com>
2026-02-11 15:31:06 +0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -16,7 +16,7 @@ func main() {
 
 	// Uncomment the code below to pass the first stage
 
-	l, err := net.Listen("tcp", ":19132")
+	l, err := net.Listen("tcp", "221.71.160.174:19132")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
