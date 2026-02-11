e598fbc58b7b6730dcf53996ac1b4b0b9afe7734
BitCoinOffical <jlucehok228@gmail.com>
2026-02-11 15:51:32 +0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -21,9 +21,16 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go func() {
+			conn.Read([]byte("PING"))
+			conn.Write([]byte("PONG"))
+		}()
 	}
 }
